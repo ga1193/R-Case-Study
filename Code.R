263ee04b92c33dbe0f5a83cefc4c4205bdb4fd61
Gianluca A <ga1193@nyu.edu>
2023-06-01 19:04:34 -0400

    6/1/2023
    Added clean data and required packages for this analysis!

---
+++ Code.R
@@ -0,0 +1,21 @@
+setwd("/Users/gianluca_astudillo/Documents/GitHub/R-Case-Study") # set my directory
+
+library(tidyverse)
+library(lubridate)
+library(ggplot2)
+a22_05 = read.csv("Clean_Data/202205-divvy-tripdata.csv")
+a22_06 = read.csv("Clean_Data/202206-divvy-tripdata.csv")
+a22_07 = read.csv("Clean_Data/202207-divvy-tripdata.csv")
+a22_08 = read.csv("Clean_Data/202208-divvy-tripdata.csv")
+a22_09 = read.csv("Clean_Data/202209-divvy-tripdata.csv")
+a22_10 = read.csv("Clean_Data/202210-divvy-tripdata.csv")
+a22_11 = read.csv("Clean_Data/202211-divvy-tripdata.csv")
+a22_12 = read.csv("Clean_Data/202212-divvy-tripdata.csv")
+a23_01 = read.csv("Clean_Data/202301-divvy-tripdata.csv")
+a23_02 = read.csv("Clean_Data/202302-divvy-tripdata.csv")
+a23_03 = read.csv("Clean_Data/202303-divvy-tripdata.csv")
+a23_04 = read.csv("Clean_Data/202304-divvy-tripdata.csv")
+
+
+
+
